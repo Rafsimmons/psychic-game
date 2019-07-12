8609cd76346db306b80ce84bcd8046e3afc8ebdd
Rafael Galvez <rfgalvez@msn.com>
2019-07-11 23:40:16 -0400

    added if statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 //variables for wins, loss, guesses left, letters guessed//
 var win=0;
 var loss=0;
-var guessesLeft= [];
+var guessesLeft= 8
 var lettersGuessed= [];
 
 // set letters to randomize//
@@ -13,5 +13,34 @@ window.onload = function() {
     var computerGuess= letters[Math.floor(Math.random() * letters.length)];
     lettersGuessed.push(computerGuess)
     console.log(computerGuess)
-
 }
+
+// player guess//
+document.onkeyup = function(event) {
+    var playerGuess= event.key;
+    lettersGuessed.push(playerGuess);
+    
+if (playerGuess === computerGuess) {
+    win++
+    lettersGuessed=[];
+  
+}
+
+if (playerGuess != computerGuess){
+    guessesLeft--
+    lettersGuessed.push(playerGuess)
+}
+if (guessesLeft ===0) {
+    loss--
+    lettersGuessed=[]
+}
+}
+var html=
+"<h2>Guess what I'm thinking of</h2>" +
+    "<p> Win: " + win + "</p" +
+    "<p>Loss: " + loss + "</p>" +
+    "<p>Guesses left: " + guessesLeft + "</p"
+    "<p>Letters Guessed: " + lettersGuessed + "</p"
+    document.querySelector("#game").text = html;
+
+
